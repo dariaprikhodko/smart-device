0eb9280a895ac080ce4e9deca695ab35ffcdd7e9
dariaprikhodko <favour313@mail.ru>
2019-09-26 14:40:53 +0900

    добавила попап и логику js

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,33 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(function () {
+  var bodyElement = document.body;
+  var htmlElement = document.querySelector('html');
+  var buttonCall = document.getElementById('order-call');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  // slider show
+  buttonCall.addEventListener('click', function () {
+    var popup = document.getElementById(modalId);
+
+    if (popup.classList.contains('visually-hidden')) {
+      popup.classList.remove('visually-hidden');
+      bodyElement.classList.add('left-padding');
+      htmlElement.classList.add('stop-scroll');
+    }
+    var buttonClose = document.querySelector('.button-close');
+    function byClose() {
+      popup.classList.add('visually-hidden');
+      popup.removeEventListener('click', byClose);
+      bodyElement.classList.remove('left-padding');
+      htmlElement.classList.remove('stop-scroll');
+    }
+    buttonClose.addEventListener('click', byClose);
+
+    document.addEventListener('keydown', function (e) {
+      if (e.keyCode === 27) {
+        byClose();
+      }
+    });
+  });
+
+})();
